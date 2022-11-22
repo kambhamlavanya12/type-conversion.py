a2487599c2fa33602d7b99cdfbad5ba8d91d7c0f
K Lavanya Reddy <117257210+kambhamlavanya12@users.noreply.github.com>
2022-11-22 17:53:13 +0530

    Add files via upload

---
+++ type-conversion.py
@@ -0,0 +1,29 @@
+a=4
+print(type(a))
+print(float(a))
+print(str(a))
+
+b=6.8
+print(type(b))
+print(int(b))
+print(str(b))
+
+a=3
+print("The value of a is",a)
+print("The value of a is ",a)
+
+b=""
+print(b,type(b))
+c="   "
+print(c.isspace())
+d=7.8
+print(type(d))
+print(int(d))
+print(float(d))
+print(str(d))
+e='67.45'
+print(e,type(e))
+g=float(e)
+print(g)
+print(int(e))
+print(int(g))
